3857c1a4757254d5fa8e6fe583a514cb01c01315
Chan Lee <chanho0325@gmail.com>
2025-08-08 22:18:58 +0900

    pstat->inuse의 타입을 _Bool로 변경

--- kernel/pstat.h
+++ kernel/pstat.h
@@ -5,7 +5,7 @@
 #include "types.h"
 
 struct pstat {
-  int inuse[NPROC];   // whether this slot of the process table is in use (1 or 0)
+  _Bool inuse[NPROC];   // whether this slot of the process table is in use (1 or 0)
   uint32 tickets[NPROC]; // the number of tickets this process has
   int pid[NPROC];     // the PID of each process 
   uint64 ticks[NPROC];   // the number of ticks each process has accumulated 
