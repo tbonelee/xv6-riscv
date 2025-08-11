6f991be71751930adfba6f8c1bfc2ca4fba14fbb
Chan Lee <chanho0325@gmail.com>
2025-08-11 23:20:46 +0900

    revert whitespaces

--- kernel/exec.c
+++ kernel/exec.c
@@ -120,7 +120,7 @@ exec(char *path, char **argv)
     if(*s == '/')
       last = s+1;
   safestrcpy(p->name, last, sizeof(p->name));
-
+    
   // Commit to the user image.
   oldpagetable = p->pagetable;
   p->pagetable = pagetable;
