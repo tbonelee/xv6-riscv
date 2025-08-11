617b4270299d2607b5aa868beadfa9bd82a74028
Chan Lee <chanho0325@gmail.com>
2025-08-11 22:27:32 +0900

    시스템콜 `mprotect()` 구현

--- kernel/defs.h
+++ kernel/defs.h
@@ -81,6 +81,7 @@ int             cpuid(void);
 void            exit(int);
 int             fork(void);
 int             shrinkproc(int);
+int             set_pages_readonly(uint64 va, uint64 npages);
 void            proc_mapstacks(pagetable_t);
 pagetable_t     proc_pagetable(struct proc *);
 void            proc_freepagetable(pagetable_t, uint64);
@@ -102,7 +103,7 @@ int             either_copyout(int user_dst, uint64 dst, void *src, uint64 len);
 int             either_copyin(void *dst, int user_src, uint64 src, uint64 len);
 void            procdump(void);
 int             getpinfo(uint64 addr);
-char            mprotect(void *addr, uint64 npages);
+int             mprotect(void *addr, uint64 npages);
 
 // swtch.S
 void            swtch(struct context*, struct context*);
